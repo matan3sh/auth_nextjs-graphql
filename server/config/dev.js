de13b3f4b6726513034b6e7f8967c1aa09195f45
Matan Shaviro <matan3sh@gmail.com>
2021-01-19 22:43:21 +0200

    Update config file

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,5 +1,4 @@
 module.exports = {
-  DB_URI:
-    'mongodb+srv://matan3sh:Ma290190@matanshaviro.tobp4.mongodb.net/matan_db?retryWrites=true&w=majority',
-  SESSION_SECRET: 'matanshaviro290190',
+  DB_URI: '',
+  SESSION_SECRET: '',
 };
